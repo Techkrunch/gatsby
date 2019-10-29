835fc8b4396174bd3e7b5d16abcc380f7ebf42bd
Techkrunch <kovinian@gmail.com>
2019-10-29 13:02:03 +0300

    first commit 1

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,13 +10,14 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import "./bootstrap.min.css";
-import "./layout.css"
+import "./layout.css";
+import Navbar from "./Globals/Navbar";
+const Layout = ({ children }) => <> <Navbar/> {children} </>;
 
-const Layout = ({ children }) => <>{children}</>;
-
-
+ 
 Layout.propTypes = {
     children: PropTypes.node.isRequired,
 }
 
 export default Layout
+
