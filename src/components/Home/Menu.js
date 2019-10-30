ca24f6f352dccb8154fcdfd59ad234a77774697a
Techkrunch <kovinian@gmail.com>
2019-10-30 17:40:43 +0300

    menu

---
+++ src/components/Home/Menu.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react"
+import Title from "../Globals/Title"
+import Img from "gatsby-image"
+import { FaBlackTie } from "react-icons/fa"
+
+export default class Manu extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      items: props.items.edges,
+      coffeeItems: props.items.edges,
+    }
+  }
+
+  render() {
+    if (this.state.items.length > 0) {
+      return (
+        <section className="menu py-5">
+          <div className="container">
+            <Title title="Best of our menu" />
+            <div className="row">
+              {/* categories start*/}
+
+              {/* categories end */}
+              {/* items start*/}
+              <div className="row>">
+                {this.state.coffeeItems.map(({ node }) => {
+                  return <div key={node.id} className="col-11 col-md-6 my-3 d-flex mx-auto">
+                 <Img fixed={node.image.fixed} />
+                  </div>   
+                })}
+              </div>
+              {/* items end */}
+            </div>
+          </div>
+        </section>
+      )
+    } else {
+      return (
+        <section className="menu py-5">
+          <div className="container">
+            <Title title="Best of our menu"></Title>
+            <div className="row">
+              <div className="col-10 col-sm-6 mx-auto text-center text capitalize">
+                <h1> There are no items to display</h1>
+              </div>
+            </div>
+          </div>
+        </section>
+      )
+    }
+  }
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,8 +3,9 @@ import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import  BackgroundSection  from "../components/Globals/BackgroundSection"
-import Info from '../components/Home/Info'
+import BackgroundSection from "../components/Globals/BackgroundSection"
+import Info from "../components/Home/Info"
+import Menu from "../components/Home/Menu"
 
 const IndexPage = ({ data }) => (
   <Layout>
@@ -14,15 +15,36 @@ const IndexPage = ({ data }) => (
       // title="TechKrunch"
       styleClass="default-background"
     />
-    <Info/>  </Layout>
+    <Info />
+    <Menu  items={data.menu}></Menu>
+  </Layout>
 )
 
 export const query = graphql`
   {
-    img: file(relativePath: { eq: "computer-1572345347195-9607.png" }) {
+    img: file(relativePath: { eq: "a.png" }) {
       childImageSharp {
         fluid {
           ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+
+    menu: allContentfulCoffeeItem {
+      edges {
+        node {
+          id
+          title
+          description {
+            description
+          }
+          price
+          category
+          image {
+            fixed(width: 100, height: 120) {
+              ...GatsbyContentfulFixed_withWebp
+            }
+          }
         }
       }
     }
